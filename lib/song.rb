ec3e053226919af8830481ebbff7718b17680dee
Parker Catalano <catalano.work@gmail.com>
2017-11-05 22:54:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,7 +54,7 @@ class Song
 
   def self.new_from_filename(string)
     trimmed = string.sub(/\.mp3/, "")
-    binding.pry
+    trimmed_array = trimmed.split("-")
     string[0]
     # string.each do |set|
     #   set = set.split
