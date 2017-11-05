b6088c2182ff9aeef2f691c0d2b996de58f91999
Parker Catalano <catalano.work@gmail.com>
2017-11-05 22:20:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,51 @@ class Song
 
   def save
     self.class.all << self
+    self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+  end
+
+  def self.new_by_name(new_name)
+    song = self.new
+    song.name=(new_name)
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name=(name)
+    song.save
+  end
+
+  def self.find_by_name(name)
+    @@all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    #   find_by_name(name)
+    # else
+    #   self.create_by_name(name)
+    # end
+
+    find_by_name(name) || create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.new_from_filename(string)
+    string = string.split
+    
   end
 
 end
